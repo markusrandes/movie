ff6404c9c14b92c1fa523115a3d9507abe106c07
Opilane TTHK <opilane@tthk.ee>
2020-11-30 15:23:30 +0200

    first commit

---
+++ filesandobjects/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace filesandobjects
+{
+    class movie
+    {
+        string title;
+        string rating;
+        string year;
+        public movie(string _title, string _rating , string _year)
+        {
+            title = _title;
+            rating = _rating;
+            year = _year;
+        }
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilane\samples";
+            string filename = @"imdb.txt";
+            List<string> movielist = File.ReadAllLines(Path.Combine(filepath, filename)).ToList();
+
+            foreach(string movieitem in movielist)
+            {
+                Console.WriteLine(movieitem);
+
+            }
+            
+            
+        }
+    }
+}
